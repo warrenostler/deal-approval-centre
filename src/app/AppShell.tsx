7b579709eb6f84c8425175acdb270419d876f20f
Warren Ostler <warren.ostler@fremantle.com>
2026-08-25 11:22:50 +0100

    Stabilize approval centre router

--- src/app/AppShell.tsx
+++ src/app/AppShell.tsx
@@ -1,25 +1,6 @@
-import { RouterProvider, createHashRouter, Outlet, useLocation } from 'react-router-dom'
+import { RouterProvider } from 'react-router-dom'
 import { appRouter } from './router'
-import { AppHeader } from './AppHeader'
-
-function ApprovalCentreLayout() {
-  const location = useLocation()
-  const showHeader = location.pathname === '/' || location.pathname === '/opportunity-history'
-
-  return (
-    <>
-      {showHeader && <AppHeader />}
-      <Outlet />
-    </>
-  )
-}
 
 export function AppShell() {
-  const router = createHashRouter([
-    {
-      element: <ApprovalCentreLayout />,
-      children: appRouter.routes,
-    },
-  ])
-  return <RouterProvider router={router} />
+  return <RouterProvider router={appRouter} />
 }
