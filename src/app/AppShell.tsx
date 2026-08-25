70ba2147bb2830bab92c937bb79e47e8b2d7169f
Warren Ostler <warren.ostler@fremantle.com>
2026-08-25 11:19:04 +0100

    Rebuild approval centre header navigation

--- src/app/AppShell.tsx
+++ src/app/AppShell.tsx
@@ -1,6 +1,25 @@
-import { RouterProvider } from 'react-router-dom'
+import { RouterProvider, createHashRouter, Outlet, useLocation } from 'react-router-dom'
 import { appRouter } from './router'
+import { AppHeader } from './AppHeader'
+
+function ApprovalCentreLayout() {
+  const location = useLocation()
+  const showHeader = location.pathname === '/' || location.pathname === '/opportunity-history'
+
+  return (
+    <>
+      {showHeader && <AppHeader />}
+      <Outlet />
+    </>
+  )
+}
 
 export function AppShell() {
-  return <RouterProvider router={appRouter} />
+  const router = createHashRouter([
+    {
+      element: <ApprovalCentreLayout />,
+      children: appRouter.routes,
+    },
+  ])
+  return <RouterProvider router={router} />
 }
